d786ba63ff57512cdbbf2da71dc4599165e6824c
gostonefire <peter.steneld@gmail.com>
2025-12-22 09:11:21 +0100

    ready for basic testing

--- scripts/master_deploy.sh
+++ scripts/master_deploy.sh
@@ -8,18 +8,47 @@ if [ -z "$REPO_NAME" ] || [ -z "$VERSION_TAG" ]; then
   exit 1
 fi
 
-cd /home/petste/Develop/"$REPO_NAME" || exit
-git fetch --tags --prune
-git checkout -f "$VERSION_TAG"
-
-cargo clean
-cargo build --release
-
-./scripts/deploy.sh
-if [ $? -ne 0 ]; then
-  exit $?
+# shellcheck disable=SC2164
+cd /home/petste/Develop/"$REPO_NAME"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not change directory to repo while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
 fi
 
-echo "Deploying $REPO_NAME with version $VERSION_TAG..."
+git fetch --tags --prune
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not fetch --tags --prune while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
+fi
 
+git checkout -f "$VERSION_TAG"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not checkout from git while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
+fi
 
+cargo clean
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not execute cargo clean while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
+fi
+
+cargo build --release
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not build release while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
+fi
+
+./scripts/deploy.sh
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "could not run repo deploy script while deploying $REPO_NAME with version $VERSION_TAG..."
+  exit $EXIT_CODE
+fi
+
+exit 0
